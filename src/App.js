ce4fea28b55dc07358bd2325f4e1dd21d6fef1b7
klaryon <klary.ardon@gmail.com>
2021-04-22 12:24:28 +0200

    joke api fetch in screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [joke, setJoke] = useState("");
+
+  useEffect(() => {
+    const fetchJoke = async () =>
+      await fetch(
+        `http://api.icndb.com/jokes/random?firstName=Klary&lastName=On`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setJoke(data.value.joke)
+          console.log(data.value.joke);
+        });
+      
+        fetchJoke();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <center>
+        <h1>The Joke Generator</h1>
+        <h2>{joke}</h2>
+        <button>Generate Joke</button>
+      </center>
     </div>
   );
 }
