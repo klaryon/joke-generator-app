f838b51231048923e81d244f50a73e1580f42fc2
klaryon <klary.ardon@gmail.com>
2021-04-22 12:49:32 +0200

    useState input onChange

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 import useRandomJoke from "./useRandomJoke";
 
 function App() {
   const joke = useRandomJoke("Klary", "On");
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   // Custom Hook
   // const joke = useRandomJoke('Klary', 'On');
@@ -12,7 +16,19 @@ function App() {
       <center>
         <h1>The Joke Generator</h1>
         <h2>{joke}</h2>
-        <button>Generate Joke</button>
+        <form>
+          <input
+            placeholder="first name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            placeholder="last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <button>Generate Joke</button>
+        </form>
       </center>
     </div>
   );
